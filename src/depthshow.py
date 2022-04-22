b068dd4f3b2e880c8eac520ed734f7253f4bebca
demons <demonssh1726@gmail.com>
2022-04-22 11:33:27 -0700

    1.2.1

--- src/depthshow.py
+++ src/depthshow.py
@@ -1,8 +1,10 @@
 
+from asyncio import InvalidStateError
 from asyncore import file_dispatcher
 from multiprocessing.connection import wait
+
+from cv2 import CV_64F
 import message_filters
-import imp
 import numpy as np
 import cv2 as cv
 from cv2 import Mat
@@ -10,10 +12,10 @@ from cv2 import waitKey
 from matplotlib import pyplot as plt
 from socket import INADDR_MAX_LOCAL_GROUP, MsgFlag
 from timeit import repeat
-import roslib
 import rospy
 from sensor_msgs.msg import Image
 from sensor_msgs.msg import CameraInfo
+from sensor_msgs.msg import PointCloud2
 import geometry_msgs.msg
 import turtlesim.srv
 import random
@@ -22,6 +24,8 @@ import cv_bridge
 from cv_bridge import CvBridge
 from cv2 import StereoSGBM
 import imp
+
+
 rospy.init_node('part3', anonymous=True)
 
 bridge = CvBridge()
@@ -62,6 +66,7 @@ def read_right(left_data,data, camera_info):
     left_disp = left_matcher.compute(grayl, grayr)
     right_disp = right_matcher.compute(grayr, grayl)
     
+
     filtered_disp_bis = grayl
     wls_filter = cv.ximgproc.createDisparityWLSFilter(left_matcher)
     wls_filter.setLambda(lmbda)
@@ -72,14 +77,30 @@ def read_right(left_data,data, camera_info):
     # cv.imshow('test', filtered_disp_vis)
     # cv.waitKey(1)
     # print("after show")
-
+    right_disp_divided = np.float32(np.divide(right_disp, 16.0))
     puber = rospy.Publisher('/zed/depth/depthsubmission', Image, queue_size = 10)
     pic = bridge.cv2_to_imgmsg(filtered_disp_vis, encoding = "passthrough")
     puber.publish(pic)
     cx = camera_info.K(2)
     cy = camera_info.K(5)
     base_line = 140
-    cx_diff = camera_info.P()
+    cx_diff = camera_info.P(2)
+    f = camera_info.K(4)
+    q = Mat((4,4), CV_64F)
+    q[0, 0] = 1
+    q[1, 1] = 1
+    q[0, 3] = -cx
+    q[1, 3] = -cy
+    q[2, 3] = f
+    q[3, 2] = (-1)/base_line
+    q[3, 3] = (cx - cx_diff)/base_line
+    _3dimage = Mat()
+
+    _3dimage = cv.reprojectImageTo3D(filtered_disp_vis, q, _3dimage, True)
+    cloud = pcl.PointCloud_PointXYZRGB()
+    for i in range (0, len(filtered_disp_vis) - 1):
+        for j in range (0, len(filtered_disp_vis[0])):
+
     rate = rospy.Rate(10)
     rate.sleep()
     
